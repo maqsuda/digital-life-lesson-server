0138ee89e796e13d74a7d0a8ab0ead29f622122d
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2025-12-22 20:11:39 +0600

    Featured lessons

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 const port = process.env.PORT || 3000;
+const crypto = require("crypto");
 
+function generateTrackingId() {
+  const prefix = "PREMIUM"; // your brand prefix
+  const date = new Date().toISOString().slice(0, 10).replace(/-/g, ""); // YYYYMMDD
+  const random = crypto.randomBytes(3).toString("hex").toUpperCase(); // 6-char random hex
+
+  return `${prefix}-${date}-${random}`;
+}
 // middleware
 app.use(express.json());
 app.use(cors());
@@ -25,12 +33,12 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const myDB = client.db("digital_life_db");
     const usersCollection = myDB.collection("users");
     const lessonsCollection = myDB.collection("lessons");
+    const paymentCollection = myDB.collection("payments");
 
     //user api
 
@@ -65,7 +73,7 @@ async function run() {
               currency: "USD",
               unit_amount: amount,
               product_data: {
-                name: paymentInfo.userName,
+                name: `Please pay for: paymentInfo.userName}`,
               },
             },
             quantity: 1,
@@ -77,7 +85,7 @@ async function run() {
           userId: paymentInfo.userId,
           userName: paymentInfo.userName,
         },
-        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
       });
 
@@ -85,7 +93,75 @@ async function run() {
       res.send({ url: session.url });
     });
 
-    //Lessons
+    app.patch("/payment-success", async (req, res) => {
+      const sessionId = req.query.session_id;
+
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+      console.log("session retrieve", session);
+      // const trackingId = generateTrackingId();
+
+      if (session.payment_status === "paid") {
+        const id = session.metadata.userId;
+        const query = { _id: new ObjectId(id) };
+        const update = {
+          $set: {
+            isPremium: true,
+            price: 1500,
+            // trackingId: trackingId,
+          },
+        };
+
+        const result = await usersCollection.updateOne(query, update);
+
+        const payment = {
+          cost: session.amount_total / 100,
+          currency: session.currency,
+          customerEmail: session.customer_email,
+          userId: session.metadata.userId,
+          userName: session.metadata.userName,
+          transactionId: session.payment_intent,
+          paymentStatus: session.payment_status,
+          paidAt: new Date(),
+        };
+
+        if (session.payment_status === "paid") {
+          const resultPayment = await paymentCollection.insertOne(payment);
+
+          res.send({
+            success: true,
+            // trackingId: trackingId,
+            transactionId: session.payment_intent,
+            paymentInfo: resultPayment,
+          });
+        }
+      }
+
+      res.send({ success: false });
+    });
+
+    //Lessons apis
+
+    app.get("/allLessons", async (req, res) => {
+      const cursor = lessonsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/featuredLessons", async (req, res) => {
+      const query = {};
+      const { isFeatured } = req.query;
+
+      // console.log(isFeatured);
+      if (isFeatured) {
+        query.isFeatured = isFeatured;
+      }
+      const options = { sort: { CreateDate: -1 } };
+      const cursor = lessonsCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     app.post("/add-lessons", async (req, res) => {
       const newUser = req.body;
       const result = await lessonsCollection.insertOne(newUser);
